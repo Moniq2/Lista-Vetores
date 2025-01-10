e8587230d66c7c1fcdecd0114dd6aab92626277d
Moniq <moniq.not43@gmail.com>
2025-01-10 09:25:27 -0300

    Q1

---
+++ Q1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+   
+    int vetor[10];
+    printf("Digite 10 numeros: \n");
+   
+    for (int i = 0; i < 10; i++)
+    {
+        scanf("%d", &vetor[i]);
+    }
+   
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+   
+    for (int j = 9; j >= 0; j--)
+    {
+        printf("%d ", vetor[j]);
+    }
+    return 0;
+}
