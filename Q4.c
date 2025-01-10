7daad426f5f79a2392941c667ec10dbc1df83484
Moniq <moniq.not43@gmail.com>
2025-01-10 10:52:18 -0300

    Q5

---
+++ Q4.c
@@ -0,0 +1,24 @@
+/* Ler um vetor com 10 nomes de pessoas, após pedir
+que o usuário digite um nome qualquer de pessoa. Escrever a mensagem “ACHEI”,
+se o nome estiver armazenado no vetor C ou “NÃO ACHEI” caso contrário.
+*/
+#include<stdio.h>
+int main(){
+char vetor[10][20];
+char nome[20];
+for (int i = 0; i < 10; i++)
+{
+scanf("%s ", &vetor[i][i]);
+}
+scanf("%d", &nome[0]);
+for (int i = 0; i < 10; i++)
+{
+if (vetor[i][i] == nome[0];)
+{
+
+}
+
+}
+ 
+return 0;
+}
---
+++ Q5.c
@@ -0,0 +1,29 @@
+/*Faça um programa que leia e monte dois vetores
+de números inteiros com 20 números cada. Depois de montados gere um terceiro
+vetor formado pela diferença dos dois vetores lidos, um quarto vetor formado
+pela soma dos dois vetores lidos e por último um quinto vetor formado pela
+multiplicação dos dois vetores lidos.
+*/
+#include <stdio.h>
+
+int main() {
+    int vetor1[20], vetor2[20], vetor3[20], vetor4[20], vetor5[20];
+    printf("Digite 20 numeros:");
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &vetor1[i]);
+    }
+    printf("Digite mais 20 numeros:");
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &vetor2[i]);
+    }
+
+    for(int i = 0; i < 5; i++)
+    {
+        vetor3[i] = vetor1[i] - vetor2[i];
+        vetor4[i] = vetor1[i] + vetor2[i];
+        vetor5[i] = vetor1[i] * vetor2[i];
+    }
+    return 0;
+}
